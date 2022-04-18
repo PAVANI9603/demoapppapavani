06c3d13276316254b2f1119d5e22d834b5aa877b
PAVANI9603 <96532411+PAVANI9603@users.noreply.github.com>
2022-04-18 18:05:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "hello world";
+// Assign the value "Hello!" to the variable "greeting"
+$greeting = "Hello This is Pavani!";
+// Assign the value 8 to the variable "month"
+$month = 4;
+// Assign the value 2019 to the variable "year"
+$year = 2022;
 ?>
